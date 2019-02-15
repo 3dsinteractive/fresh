83f377c81ddb2890f489e24eebd27203188129c8
pong3ds <chaiyapong@3dsinteractive.com>
2019-02-15 17:14:10 +0700

    Increase limit to 100000

--- runner/limit_unix.go
+++ runner/limit_unix.go
@@ -3,14 +3,14 @@
 package runner
 
 import (
-	"syscall"
 	"fmt"
+	"syscall"
 )
 
 func initLimit() {
 	var rLimit syscall.Rlimit
-	rLimit.Max = 10000
-	rLimit.Cur = 10000
+	rLimit.Max = 100000
+	rLimit.Cur = 100000
 	err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
 	if err != nil {
 		fmt.Println("Error Setting Rlimit ", err)
